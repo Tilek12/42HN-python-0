e49303af468a9bb9aa20263380111c35b3ad560e
Tilek Kubanychbekov <tkubanyc@3-F-13.42heilbronn.de>
2026-04-17 14:47:41 +0200

    Update knowledge.md. Finish ex00

--- ex00/Hello.py
+++ ex00/Hello.py
@@ -3,7 +3,11 @@ ft_tuple = ("Hello", "toto!")
 ft_set = {"Hello", "tutu!"}
 ft_dict = {"Hello" : "titi!"}
 
-#your code here
+ft_list[1] = "World!"
+ft_tuple = ("Hello", "Germany!")
+ft_set.remove("tutu!")
+ft_set.add("Heilbronn!")
+ft_dict["Hello"] = "42Heilbronn!"
 
 print(ft_list)
 print(ft_tuple)
